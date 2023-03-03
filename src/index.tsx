18d1d77b8283c5dc477bea1a4e8d391372be8e04
gtgambit <gtgambit@gmail.com>
2023-03-03 16:53:36 +0200

    updates to json and index ts BrowseRouter

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/alterego-test/">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
