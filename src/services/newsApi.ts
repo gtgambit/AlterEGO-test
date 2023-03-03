2fb2d145da18bbcfecae4b6463d9d1b8e11ecfde
gtgambit <gtgambit@gmail.com>
2023-03-03 17:43:27 +0200

    updates proxy URL for CORS

--- src/services/newsApi.ts
+++ src/services/newsApi.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+
 export const fetchArticles = async (page: number) => {
   const { data } = await axios.get(
-    `https://newsapi.org/v2/everything?q=apple&from=2023-02-28&to=2023-02-28&page=${page}&pagesize=10&sortBy=popularity&apiKey=b6225402ed9746ef8e861d9757e1c93b`
+    `${PROXY_URL}https://newsapi.org/v2/everything?q=apple&from=2023-02-28&to=2023-02-28&page=${page}&pagesize=10&sortBy=popularity&apiKey=b6225402ed9746ef8e861d9757e1c93b`,
+    { headers: { "X-Requested-With": "XMLHttpRequest" } }
   );
   return data;
 };
