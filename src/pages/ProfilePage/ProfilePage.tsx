1d6400a52b48ec1aab93ff206d46cad7cbf21638
gtgambit <gtgambit@gmail.com>
2023-03-07 11:38:41 +0200

    ProfilePage refactor for mobile devices

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,57 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Avatar,
   Box,
+  Container,
   List,
   ListItem,
   ListItemText,
   Typography,
-  Container,
 } from "@mui/material";
-import { useEffect, useState } from "react";
 import { withAuthRedirect } from "../../hoc/WithAuthRedirect";
 import { getUserAvatar } from "../../services/profileApi";
 import { useTranslation } from "react-i18next";
 
-const styles = {
-  root: {
-    marginTop: 4,
-    display: "flex",
-    alignItems: "center",
-    marginBottom: 16,
-  },
-  avatar: {
-    width: 128,
-    height: 128,
-    marginRight: 24,
-  },
-  name: {
-    fontWeight: "bold",
-  },
-  stats: {
-    display: "flex",
-    marginTop: 16,
-    "& li": {
-      marginRight: 32,
-    },
-  },
-};
-
-type userProps = {
-  email: string;
-  first_name: string;
-  avatar: string;
-};
-
 const ProfilePage = () => {
-  const [profile, setProfile] = useState<userProps | null>(null);
+  const [profile, setProfile] = useState({
+    email: "",
+    first_name: "",
+    avatar: "",
+  });
 
   const { t } = useTranslation();
 
   const getUser = async () => {
     try {
       const data = await getUserAvatar();
-      return setProfile(data);
+      setProfile(data);
     } catch (err) {
       console.log(err);
     }
@@ -63,29 +36,56 @@ const ProfilePage = () => {
 
   return (
     <Container maxWidth="lg">
-      <Box style={styles.root}>
+      <Box
+        sx={{
+          marginTop: 2,
+          display: { xs: "block", md: "flex" },
+          alignItems: "center",
+          marginBottom: 16,
+        }}>
         <Avatar
           alt="Profile Avatar"
           src={
-            profile?.avatar
-              ? profile.avatar
-              : "https://cdn.icon-icons.com/icons2/3310/PNG/512/student_man_avatar_user_toga_school_university_icon_209264.png"
+            profile.avatar ||
+            "https://cdn.icon-icons.com/icons2/3310/PNG/512/student_man_avatar_user_toga_school_university_icon_209264.png"
           }
-          style={styles.avatar}
+          sx={{
+            width: 128,
+            height: 128,
+            margin: " auto",
+          }}
         />
         <Box>
-          <Typography variant="h4" style={styles.name}>
-            {profile?.first_name}
+          <Typography
+            variant="h4"
+            sx={{
+              fontWeight: "bold",
+              textAlign: { xs: "center", md: "normal" },
+            }}>
+            {profile.first_name}
           </Typography>
-          <Typography variant="body1">{t("profile.text")}</Typography>
-          <List style={styles.stats}>
-            <ListItem>
+          <Typography
+            variant="body1"
+            sx={{
+              marginRight: { xs: "0", md: "16" },
+              textAlign: { xs: "center", md: "" },
+            }}>
+            {t("profile.text")}
+          </Typography>
+          <List sx={{ display: { xs: "block", md: "flex" }, marginTop: 0 }}>
+            <ListItem
+              sx={{ marginRight: { xs: "0", md: "16" }, textAlign: "center" }}>
               <ListItemText primary={t("profile.posts")} secondary="20" />
             </ListItem>
-            <ListItem>
+            <ListItem
+              sx={{
+                marginRight: { xs: "0", md: "16" },
+                textAlign: "center",
+              }}>
               <ListItemText primary={t("profile.followers")} secondary="100" />
             </ListItem>
-            <ListItem>
+            <ListItem
+              sx={{ marginRight: { xs: "0", md: "16" }, textAlign: "center" }}>
               <ListItemText primary={t("profile.followings")} secondary="50" />
             </ListItem>
           </List>
